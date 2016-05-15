f89e61fadd8ccf8dd5ffc71e44fe108391e1e569
Ryan T. Hosford <tad.hosford@gmail.com>
2016-05-15 12:59:29 -0500

    Sheets, robots no crawl

---
+++ app/views/sheets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @sheet, :id, :url, :title, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513082819) do
+ActiveRecord::Schema.define(version: 20160513235744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160513082819) do
     t.text     "page_image_link_target"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "sheets", force: :cascade do |t|
+    t.text     "url"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
