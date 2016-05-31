d80a9cd9845e4d414ceb1d6b76981e94ded805c0
Ryan T. Hosford <tad.hosford@gmail.com>
2016-05-30 21:22:01 -0500

    Bugfixing

--- app/helpers/sheets_helper.rb
+++ app/helpers/sheets_helper.rb
@@ -1,9 +1,9 @@
 module SheetsHelper
-  def tweet_sheet_text
-    if @sheet.tweet_override.present?
-      @sheet.tweet_override
+  def tweet_sheet_text(sheet)
+    if sheet.tweet_override.present?
+      sheet.tweet_override
     else
-      @sheet.onda.tweet_text if @sheet.onda
+      sheet.onda.tweet_text if sheet.onda
     end
   end
 end
